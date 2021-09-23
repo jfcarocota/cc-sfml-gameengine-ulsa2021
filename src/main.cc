285bdeb04aea4eab51fdbc74924285b3b2f1e93d
jfcarocota <jfcarocota@gmail.com>
2021-09-23 08:57:09 -0700

    sfml added

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,25 @@
 #include<iostream>
+#include<SFML/Graphics.hpp>
 
 int main()
 {
-  std::cout << "Hello sfml" << std::endl;
-  std::cin.get();
-  return 0;
+  sf::RenderWindow* window{new sf::RenderWindow(sf::VideoMode(800, 800), "Game Engine")};
+  sf::Event* event{new sf::Event()};
+
+  //Loop principal
+  while (window->isOpen())
+  {
+    while (window->pollEvent(*event))
+    {
+      if(event->type == sf::Event::Closed)
+      {
+        window->close();
+      }
+    }
+
+    //Update
+
+  }
+
+  return EXIT_SUCCESS;
 }
